581711d4d147ece3eebbb1cb6a658349e4702336
TAKO9977 <116018605+TAKO9977@users.noreply.github.com>
2022-12-19 03:46:55 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+// problem 1
+let fullname ="tamar izoria";
+console.log(fullname);
+
+let gender ="female";
+console.log(gender);
+
+let age ="26"
+console.log(age);
+
+let birthdate = "1995";
+console.log(birthdate);
+
+//problem 2
+(100 - (10+15)/5)*3; // 9 Expression;
+1.10+15
+2.10+15/5
+3.(10+15)/5
+4.(10+15)/5*3
+5.(10+15/5)*3
+6.100 - (10+15)/5
+7.(100 - (10+15)/5)*3
+8.100 - (10+15)/5*3
+9.100 - 10+15/5*3
+
+
+
+// problem 3
+let result = 101 / 2;
+console.log(result);
+
+
+
